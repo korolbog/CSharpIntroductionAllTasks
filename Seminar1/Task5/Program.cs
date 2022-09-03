9bcc223e1e6b3009b0219a50be717ee9271ce8c5
BogdanKorolev <bkorolevjob@gmail.com>
2022-09-03 19:21:21 +0300

    Task5 solved

---
+++ Seminar1/Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 5
+Напишите программу вычисления значения функции возведения
+числа в квадрат.
+*/
+Task5();
+
+void Task5()
+{
+    int randomNumber = new Random().Next(-10000,10001);
+    Console.WriteLine($"Ваше случайное число: {randomNumber}");
+    Console.WriteLine($"Квадрат числа: {Math.Pow(randomNumber, 2)}");
+}
