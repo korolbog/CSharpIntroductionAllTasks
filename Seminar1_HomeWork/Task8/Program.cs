cd287741e7e91617ad178a43981538db429e7397
BogdanKorolev <bkorolevjob@gmail.com>
2022-09-04 21:13:59 +0300

    Task8 solved

---
+++ Seminar1_HomeWork/Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 8
+Напишите программу, которая на вход принимает число (N), а на
+выходе показывает все чётные числа от 1 до N.
+*/
+
+int number = new Random().Next(1,11);
+int i = 1;
+string result = String.Empty;
+
+Console.WriteLine($"Ваше случайное число: {number}");
+if (number < 2)
+{
+    Console.WriteLine("Четных чисел нет");
+    return;
+}
+else while (i <= number)
+{
+    if (i % 2 == 0)
+    {
+        result += ($"{i} "); 
+    }
+    i++;
+}
+Console.WriteLine($"Четные числа от 1 до {number}:");
+Console.WriteLine(result);
