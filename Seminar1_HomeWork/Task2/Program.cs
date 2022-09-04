987cd5931591fbd69c8c73cc24b54eee73aa5a49
BogdanKorolev <bkorolevjob@gmail.com>
2022-09-04 20:10:58 +0300

    Task 4 solved

---
+++ Seminar1_HomeWork/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 2
+Напишите программу, которая на вход принимает два числа и
+выдаёт, какое число большее, а какое меньшее.
+*/
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.Write($"{firstNumber} - большее, {secondNumber} - меньшее");
+}
+else
+{
+    Console.Write($"{secondNumber} - большее, {firstNumber} - меньшее");
+}
---
+++ Seminar1_HomeWork/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 4
+Напишите программу, которая принимает на вход три числа и
+выдаёт максимальное из этих чисел.
+*/
+
+int firstNumber = new Random().Next(1,11);
+int secondNumber = new Random().Next(1,11);
+int thirdNumber = new Random().Next(10,11);
+int maxNumber = firstNumber;
+
+Console.WriteLine($"Ваши три числа: {firstNumber} \t{secondNumber} \t {thirdNumber}");
+if (secondNumber > maxNumber)
+{
+    maxNumber = secondNumber;
+}
+if (thirdNumber > maxNumber)
+{
+    maxNumber = thirdNumber;
+}
+Console.WriteLine($"Максимальное число из трёх: {maxNumber}");
