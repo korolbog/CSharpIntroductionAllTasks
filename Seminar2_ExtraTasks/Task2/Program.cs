ad3d655de377b8724dc2b2b8049a4d690113a003
BogdanKorolev <bkorolevjob@gmail.com>
2022-09-05 17:33:14 +0300

    Task 2 solved.

---
+++ Seminar2_ExtraTasks/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Даны два числа a, b, такие что a < b. Вывести на экран сколько раз
+число a поместится в числе b
+*/
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+int a = new Random().Next(1,51);
+int b = new Random().Next(51,101);
+Console.WriteLine($"Число a = {a} поместится в число b = {b} следующее количество раз: {b/a}");
+watch.Stop();
+Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
